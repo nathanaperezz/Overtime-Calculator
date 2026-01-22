4305adc8543748b60735a127cd1ee9998f87fd10
Nathan Perez <naperezmail@gmail.com>
2026-01-21 19:58:28 -0500

    Initial Commit

---
+++ WageCalculator/WageCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  WageCalculatorApp.swift
+//  WageCalculator
+//
+//  Created by  Nathan Perez on 1/21/26.
+//
+
+import SwiftUI
+
+@main
+struct WageCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
